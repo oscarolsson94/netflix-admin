d04606b1f2bc2eb38a2005460fcec147d47eda3a
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-20 22:47:43 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import NewList from "./pages/newList/NewList";
 function App() {
   const { user } = useContext(AuthContext);
   return (
-    <Router basename="/">
+    <Router>
       <Switch>
         <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
         {user && (
